a7cba06f6ef7a0e8db21470f1cba2c66afdfe5af
Adam Watters <adamwatters11@gmail.com>
2019-05-15 10:24:07 -0400

    search dictionary with fuse

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import namedColors from "color-name-list";
+import Fuse from "fuse.js";
+
+const fuse = new Fuse(namedColors, {
+  shouldSort: true,
+  threshold: 0.1,
+  location: 0,
+  distance: 100,
+  maxPatternLength: 16,
+  minMatchCharLength: 1,
+  keys: ["name"]
+});
 
 function App() {
+  const [search, setSearch] = useState("");
+  const handleSearchChange = ({ target: { value } }) => {
+    setSearch(value);
+  };
+
+  const colors = fuse.search(search).slice(0, 60);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        style={{ width: 300, height: 60, fontSize: 40, margin: 20 }}
+        type="text"
+        onChange={handleSearchChange}
+        value={search}
+      />
+      <div>
+        {colors.map(color => {
+          return (
+            <div key={color.hex} style={{ backgroundColor: color.hex }}>
+              {color.name}
+            </div>
+          );
+        })}
+      </div>
+      {}
     </div>
   );
 }
