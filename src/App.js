9cf9dddca1ad4a73bfd2aa10ccc992578ffba89a
Adam Watters <adamwatters11@gmail.com>
2019-05-15 10:50:42 -0400

    tab through colors

--- src/App.js
+++ src/App.js
@@ -5,13 +5,39 @@ import Fuse from "fuse.js";
 
 const fuse = new Fuse(namedColors, {
   shouldSort: true,
-  threshold: 0.1,
+  threshold: 0.2,
   location: 0,
   distance: 100,
   maxPatternLength: 16,
   minMatchCharLength: 1,
   keys: ["name"]
 });
+
+function Color({ color }) {
+  const [focus, setFocus] = useState(false);
+  return (
+    <div
+      tabindex="0"
+      key={color.hex}
+      style={{
+        backgroundColor: color.hex,
+        height: focus ? 35 : 20,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        transition: "height .2s"
+      }}
+      onFocus={() => {
+        setFocus(true);
+      }}
+      onBlur={() => {
+        setFocus(false);
+      }}
+    >
+      {color.name}
+    </div>
+  );
+}
 
 function App() {
   const [search, setSearch] = useState("");
@@ -30,13 +56,9 @@ function App() {
         value={search}
       />
       <div>
-        {colors.map(color => {
-          return (
-            <div key={color.hex} style={{ backgroundColor: color.hex }}>
-              {color.name}
-            </div>
-          );
-        })}
+        {colors.map(color => (
+          <Color color={color} />
+        ))}
       </div>
       {}
     </div>
